b5324ca1b962223abce0534b151f466c3f96dc7c
LAPTOP-H7PMPE65\xc <347691082@qq.com>
2018-01-18 11:17:51 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastclick from 'fastclick'
-
 import axios from 'axios'
 
 Vue.prototype.$http = axios
@@ -10,8 +8,6 @@ Vue.prototype.$http = axios
 Vue.prototype.$goRoute = function (index) {
   this.$router.push(index)
 }
-
-fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
